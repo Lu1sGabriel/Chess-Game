2965c0c712e1460ac9385e0dd2c1aee775686ca6
Luis Gabriel Goés <luisgabriel533@gmail.com>
2024-05-24 23:19:29 -0300

    Criação da classe Position

---
+++ src/main/java/Program.java
@@ -0,0 +1,10 @@
+package src.main.java;
+
+import src.main.java.boardGame.Position;
+
+public class Program {
+    public static void main(String[] args) {
+        var position = new Position(3, 5);
+        System.out.println(position);
+    }
+}
