7be977b0ef03c8050651d4909e05747c70dbccb8
Luis Gabriel Goés <luisgabriel533@gmail.com>
2024-05-26 16:34:54 -0300

    Criação da lógica de movimento do cavalo

---
+++ src/main/java/chess/pieces/Knight.java
@@ -0,0 +1,93 @@
+package src.main.java.chess.pieces;
+
+import src.main.java.boardGame.Board;
+import src.main.java.boardGame.Position;
+import src.main.java.chess.ChessPiece;
+import src.main.java.chess.Color;
+
+import java.util.Objects;
+
+/**
+ * Classe Knight que representa a peça de Cavalo no jogo de xadrez.
+ * Esta classe herda de ChessPiece.
+ */
+public class Knight extends ChessPiece {
+
+    /**
+     * Matriz de direções possíveis para o movimento do Cavalo.
+     * O Cavalo pode se mover em oito direções diferentes, combinando 2 casas em uma direção e 1 casa em uma direção perpendicular.
+     */
+    private static final int[][] DIRECTIONS = {
+            {-2, -1},
+            {-2, 1},
+            {-1, -2},
+            {-1, 2},
+            {1, -2},
+            {1, 2},
+            {2, -1},
+            {2, 1}
+    };
+
+    /**
+     * Construtor da classe Knight.
+     *
+     * @param board Tabuleiro do jogo.
+     * @param color Cor da peça.
+     */
+    public Knight(Board board, Color color) {
+        super(Objects.requireNonNull(board, "O tabuleiro não pode ser nullo. "),
+                Objects.requireNonNull(color, "A cor não pode ser nulla. "));
+    }
+
+    /**
+     * Calcula os movimentos possíveis para o Cavalo.
+     *
+     * @return Matriz booleana indicando os movimentos possíveis.
+     */
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] validMoves = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        for (int[] moveDirection : DIRECTIONS) {
+            checkDirection(validMoves, moveDirection[0], moveDirection[1]);
+        }
+
+        return validMoves;
+    }
+
+    /**
+     * Verifica uma direção específica para os movimentos possíveis do Cavalo.
+     *
+     * @param validMoves Matriz de movimentos válidos a ser preenchida.
+     * @param rowOffset  Deslocamento na direção da linha.
+     * @param colOffset  Deslocamento na direção da coluna.
+     */
+    private void checkDirection(final boolean[][] validMoves, final int rowOffset, final int colOffset) {
+        var currentPosition = new Position(position.getRow() + rowOffset, position.getColumn() + colOffset);
+        if (getBoard().positionExists(currentPosition) && canMove(currentPosition)) {
+            validMoves[currentPosition.getRow()][currentPosition.getColumn()] = true;
+        }
+    }
+
+    /**
+     * Verifica se o Cavalo pode se mover para uma nova posição.
+     *
+     * @param newPosition A nova posição para a qual o Cavalo pode se mover.
+     * @return Verdadeiro se o Cavalo pode se mover para a nova posição, falso caso contrário.
+     */
+    private boolean canMove(final Position newPosition) {
+        var pieceAtNewPosition = (ChessPiece) getBoard().piece(newPosition);
+        return pieceAtNewPosition == null || pieceAtNewPosition.getColor() != getColor();
+    }
+
+    /**
+     * Retorna a representação em String do Cavalo.
+     *
+     * @return Uma string "K" que representa o Cavalo.
+     */
+    @Override
+    public String toString() {
+        return "N";
+    }
+
+}
